df91273760955406bdab05c87be00ea3868a982c
pboholst <pboholst@BoholStreet.noreply.github.com>
2019-09-14 14:43:15 -0700

    Update ReadingAppConfigFile.cs
    added to notes

--- AppConfigFile/ReadingAppConfigFile.cs
+++ AppConfigFile/ReadingAppConfigFile.cs
@@ -17,7 +17,7 @@ namespace BoholStreet.AppConfigFile
         /* Prerequisite
          *  1. Reference -> Add Reference -> Assemblies  -> System.Configuration
          *  2. using System.Configuration;
-         *  3. using
+         *  3. using System.Collections.Specialized;
          *  4. In App.config file add
          *      <configuration>
          *          <appSettings>
