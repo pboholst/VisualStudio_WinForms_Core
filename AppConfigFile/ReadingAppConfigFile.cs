54ead45634ef96bbd21f7e04e8a937de53631b24
pboholst <pboholst@BoholStreet.noreply.github.com>
2019-09-14 14:36:24 -0700

    Initial commit

---
+++ AppConfigFile/ReadingAppConfigFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoholStreet.AppConfigFile
+{
+    public class ReadingAppConfigFile
+    {
+        public ReadingAppConfigFile()
+        {
+            MethodName();
+        }
+        /* Prerequisite
+         *  1. Reference -> Add Reference -> Assemblies  -> System.Configuration
+         *  2. using System.Configuration;
+         *  3. using
+         *  4. In App.config file add
+         *      <configuration>
+         *          <appSettings>
+         *              <add key="key one" value="the value of 1"/>
+         *              <add key="key two" value="the value of 2"/>
+         *              <add key="key three" value="the value of 3"/>
+         *          </appSettings>
+         *      </configuration>
+         * **/
+
+        public static void MethodName()
+        {
+            // Read one of the keys from app.config file
+            string s1 = ConfigurationManager.AppSettings["key one"];
+            Console.WriteLine(s1);
+
+            // Read all the keys from the app.config file
+            NameValueCollection sAll = ConfigurationManager.AppSettings;
+
+            foreach (string s in sAll.AllKeys)
+                Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# VisualStudio_WinForms_LookUp
+ Win Forms Core codes for quick references and samples
